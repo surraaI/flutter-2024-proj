21bbe96883d121f3da8ceab241ab7c892620b9af
Roman kebede <romankebede69@gmail.com>
2024-04-17 21:07:13 +0000

    first commit

---
+++ prsenal_health_tracker/lib/components/text_field.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class MyTextField extends StatelessWidget {
+  final controller;
+  final String hintText;
+  final bool obscureText;
+  const MyTextField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40.0),
+              child: TextField(
+                controller: controller,
+                obscureText: obscureText,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 15.0),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                    borderRadius: BorderRadius.all(Radius.circular(30))
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                    borderRadius: BorderRadius.all(Radius.circular(30))
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey.shade200,
+                  hintText: hintText,
+                ),
+              
+              ),
+              );
+  }
+}
